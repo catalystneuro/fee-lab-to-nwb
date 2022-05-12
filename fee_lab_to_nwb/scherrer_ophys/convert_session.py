b40fa969a4c96c257c3fc1bae668dd4bc1180efc
weiglszonja <wszonja@gmail.com>
2022-05-12 17:21:52 +0200

    process behavior data from csv

--- fee_lab_to_nwb/scherrer_ophys/convert_session.py
+++ fee_lab_to_nwb/scherrer_ophys/convert_session.py
@@ -2,27 +2,29 @@
 from pathlib import Path
 
 from nwb_conversion_tools.utils import load_dict_from_file, dict_deep_update
-from fee_lab_to_nwb.scherrer_ophys import ScherrerOphysDatasetNWBConverter
+from fee_lab_to_nwb.scherrer_ophys import ScherrerOphysNWBConverter
 
-example_path = Path("D:/ExampleNWBConversion")
-example_session_id = example_path.stem
-nwbfile_path = example_path / f"{example_session_id}.nwb"
+ophys_dataset_path = Path("../../data/")
+ophys_dataset_session_id = ophys_dataset_path.stem
+nwbfile_path = ophys_dataset_path / f"{ophys_dataset_session_id}.nwb"
 
-metadata_path = Path(__file__) / "scherrer_ophys_metadata.yaml"
+metadata_path = Path(__file__).parent / "scherrer_ophys_metadata.yml"
 metadata_from_yaml = load_dict_from_file(metadata_path)
 
 source_data = dict(
-    Recording=dict(),
-    LFP=dict(),
-    Sorting=dict(),
-    Behavior=dict(),
+    #Recording=dict(),
+    #LFP=dict(),
+    #Sorting=dict(),
+    Behavior=dict(
+        behavior_data_file_path=str(
+            ophys_dataset_path / "home_pos-speed-in_2021-06-03T11_46_29.csv"
+        )
+    ),
 )
 
-ophys_dataset_converter = ScherrerOphysDatasetNWBConverter(source_data=source_data)
+ophys_dataset_converter = ScherrerOphysNWBConverter(source_data=source_data)
 
 metadata = ophys_dataset_converter.get_metadata()
 metadata = dict_deep_update(metadata, metadata_from_yaml)
 
-ophys_dataset_converter.run_conversion(
-    metadata=metadata, nwbfile_path=nwbfile_path
-)
+ophys_dataset_converter.run_conversion(metadata=metadata, nwbfile_path=nwbfile_path)
--- fee_lab_to_nwb/scherrer_ophys/scherrerophysbehaviorinterface.py
+++ fee_lab_to_nwb/scherrer_ophys/scherrerophysbehaviorinterface.py
@@ -1,18 +1,61 @@
 """Primary class defining conversion of experiment-specific behavior."""
+import uuid
+from datetime import datetime
+from zoneinfo import ZoneInfo
+from pathlib import Path
+
+from pandas import read_csv, to_datetime
+
 from nwb_conversion_tools.basedatainterface import BaseDataInterface
+from pynwb import NWBFile
+from pynwb.behavior import SpatialSeries
 
 
 class ScherrerOphysBehaviorInterface(BaseDataInterface):
-    """My behavior interface specific to the ecephys experiments."""
+    """Data interface for behavioral data."""
 
-    def __init__(self):
-        # Point to data
-        pass
+    def __init__(
+        self,
+        behavior_data_file_path: str,
+    ):
+        super().__init__()
+        self.behavior_data_file_path = Path(behavior_data_file_path)
+
+        assert (
+            self.behavior_data_file_path.suffix == ".csv"
+        ), f"{behavior_data_file_path} should be a .csv"
+        assert (
+            self.behavior_data_file_path.exists()
+        ), f"{behavior_data_file_path} does not exist"
+
+        # first column is timestamps for frames in behavior video, then by x, y position
+        # following the timestamp is additional tracking data, which can be ignored
+        self.behavior_data = read_csv(
+            self.behavior_data_file_path, sep=" ", header=None, usecols=[0, 1, 2]
+        )
 
     def get_metadata(self):
-        # Automatically retrieve as much metadata as possible
-        pass
+        """Extracts metadata from the behavior data file name"""
+        file_name = self.behavior_data_file_path.stem
+        ind = [i for i, c in enumerate(file_name) if c.isdigit()][0]
+        date_from_file_name = file_name[ind:]
 
-    def run_conversion(self):
-        # All the custom code to write through PyNWB
-        pass
+        session_start_time = datetime.strptime(date_from_file_name, "%Y-%m-%dT%H_%M_%S")
+        session_start_time = session_start_time.replace(tzinfo=ZoneInfo("US/Eastern"))
+
+        metadata = dict(
+            NWBFile=dict(
+                identifier=str(uuid.uuid4()),
+                session_start_time=str(session_start_time),
+            )
+        )
+
+        return metadata
+
+    def run_conversion(
+        self, nwbfile: NWBFile, metadata: dict = None, overwrite: bool = False
+    ):
+        timestamp_in_datetime = to_datetime(self.behavior_data[0])
+        elapsed_time_since_start = timestamp_in_datetime - timestamp_in_datetime.min()
+        timestamps = elapsed_time_since_start.apply(lambda x: x.total_seconds())
+        data = self.behavior_data[[1, 2]].to_numpy()
