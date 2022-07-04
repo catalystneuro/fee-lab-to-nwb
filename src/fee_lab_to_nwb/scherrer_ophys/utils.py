aedf35ad6cebe264060730bdd9ca1b56a8ce2cd9
weiglszonja <wszonja@gmail.com>
2022-07-04 14:23:06 +0200

    refactor utils method

--- src/fee_lab_to_nwb/scherrer_ophys/utils.py
+++ src/fee_lab_to_nwb/scherrer_ophys/utils.py
@@ -1,13 +1,17 @@
 """Utility functions for this dataset."""
 from pathlib import Path
 
+from nwb_conversion_tools.utils import FilePathType
 from pandas import read_csv, to_datetime
 
 
-def get_timestamps_from_csv(file_path: Path):
+def get_timestamps_from_csv(file_path: FilePathType):
     """
     Extracts timestamps from a file.
     """
+
+    if isinstance(file_path, str):
+        file_path = Path(file_path)
 
     assert file_path.suffix == ".csv", f"{file_path} should be a .csv"
     assert file_path.exists(), f"{file_path} does not exist"
