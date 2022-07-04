04cdc1b96be3cedf9c1f70862daaae78ccca962d
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2022-07-04 10:08:06 -0400

    Update src/fee_lab_to_nwb/scherrer_ophys/scherrerophysimagingextractorinterface.py

--- src/fee_lab_to_nwb/scherrer_ophys/scherrerophysimagingextractorinterface.py
+++ src/fee_lab_to_nwb/scherrer_ophys/scherrerophysimagingextractorinterface.py
@@ -21,7 +21,6 @@ class ScherrerOphysImagingExtractorInterface(BaseImagingExtractorInterface):
             for file_path in ophys_file_paths
         ]
         super().__init__(imaging_extractors=imaging_extractors)
-        self.imaging_extractor = self.IX(imaging_extractors=imaging_extractors)
         timestamps = get_timestamps_from_csv(file_path=timestamps_file_path)
         if not calculate_regular_series_rate(timestamps):
             # only use timestamps if they are not regular
