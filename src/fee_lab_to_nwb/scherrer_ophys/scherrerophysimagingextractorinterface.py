7142e6d454fbaadb24f46d0943b1312a1b5d6ef3
Szonja Weigl <wszonja@gmail.com>
2022-07-04 13:49:55 +0200

    Update src/fee_lab_to_nwb/scherrer_ophys/scherrerophysimagingextractorinterface.py
    Co-authored-by: Ben Dichter <ben.dichter@gmail.com>

--- src/fee_lab_to_nwb/scherrer_ophys/scherrerophysimagingextractorinterface.py
+++ src/fee_lab_to_nwb/scherrer_ophys/scherrerophysimagingextractorinterface.py
@@ -7,8 +7,8 @@ from roiextractors.multiimagingextractor import MultiImagingExtractor
 class ScherrerOphysImagingExtractorInterface(BaseImagingExtractorInterface):
     IX = MultiImagingExtractor
 
-    def __init__(self, imaging_extractors: list, timestamps: list):
+    def __init__(self, imaging_extractors: list, timestamps: list, verbose: bool = True):
         super().__init__(imaging_extractors=imaging_extractors)
         self.imaging_extractor = self.IX(imaging_extractors=imaging_extractors)
         self.imaging_extractor.set_times(times=timestamps)
-        self.verbose = True
+        self.verbose = verbose
