aea9f2e3da699723a3cd0ef534e3faab3f32233a
weiglszonja <wszonja@gmail.com>
2022-08-29 13:54:16 +0200

    add separate interface for addig timing of motifs as trials to nwb file

--- src/fee_lab_to_nwb/happ_ecephys/convert_session.py
+++ src/fee_lab_to_nwb/happ_ecephys/convert_session.py
@@ -27,10 +27,14 @@ raw_file_path = (
 lfp_file_path = raw_file_path.parent / raw_file_path.name.replace("ap", "lf")
 # nidq_file_path = str(experiment_folder / f"{session_name}_t0.nidq.bin")
 
+# the file path to the mat file with motif data
+motif_file_path = experiment_folder / "timingData_210729.mat"
+
 source_data = dict(
     SpikeGLXRecording=dict(file_path=str(raw_file_path)),
     SpikeGLXLFP=dict(file_path=str(lfp_file_path)),
     Sorting=dict(folder_path=str(raw_file_path.parent)),
+    Motif=dict(file_path=str(motif_file_path))
 )
 
 conversion_options = dict(
